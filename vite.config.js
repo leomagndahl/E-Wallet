f1f227ad4049d3ec29e188836072185603827ecf
Leo Windows <leobmagndahl@gmail.com>
2023-09-26 22:49:03 +0200

    Maybe pages work now?

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import generouted from "@generouted/react-router/plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/E-Wallet",
   plugins: [react(), generouted()],
 });
